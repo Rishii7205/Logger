0804c62f5d34559c9d013d8754065ae04520aa22
Gᴏᴅғᴀᴛʜᴇʀ <104144725+mrtosumemon@users.noreply.github.com>
2023-05-16 14:29:01 +0530

    Update share_text.py

--- plugins/ExraMods/share_text.py
+++ plugins/ExraMods/share_text.py
@@ -16,7 +16,7 @@ async def share_text(client, message):
         await message.reply_text(
             text=f"**Notice:**\n\n1. Reply Any Messages.\n2. No Media Support\n\n**Any Question Join Support Chat**",                
             reply_to_message_id=reply_id,               
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("😁😁", url=f"https://t.me/THE_VIP_BOY")]])
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("😁😁", url=f"https://t.me/INCRICIBLE")]])
             )                                                   
         return
     await message.reply_text(
